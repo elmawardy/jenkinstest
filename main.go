592187412ce49fdf7f2162a027988ab13547d4df
root <root@ansible-control.must.edu.eg>
2019-12-17 15:57:10 +0200

    in the name of Allah

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+import "net/http"
+import "log"
+
+func main() {
+  for i:=0;i<10;i++{
+	fmt.Println("Allah Akbar")
+  }
+ 
+   http.HandleFunc("/", handler)
+   log.Fatal(http.ListenAndServe(":8081", nil))
+
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
